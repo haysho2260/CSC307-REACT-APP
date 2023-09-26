38355e80d0e973bfbffbac3891055237274040a8
Hayley <haysho2260@gmail.com>
2023-09-26 15:44:10 -0700

    add use state so that page can update on click

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,5 +1,5 @@
 // src/MyApp.js
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
 // src/MyApp.js
@@ -23,9 +23,24 @@ const characters = [
 ];
 
 function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor",
+    },
+    // the rest of the data
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
   return (
     <div className="container">
-      <Table characterData={characters} />
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
     </div>
   );
 }
