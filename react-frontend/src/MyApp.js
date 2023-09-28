f87cbbee978c3dd05599ae36b7095bbbdd1ece11
Hayley <haysho2260@gmail.com>
2023-09-27 17:58:29 -0700

    use hooks to dynamically update website with new texts and deletes

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,29 +1,11 @@
 // src/MyApp.js
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
-// src/MyApp.js
-const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor",
-  },
-  {
-    name: "Mac",
-    job: "Bouncer",
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress",
-  },
-  {
-    name: "Dennis",
-    job: "Bartender",
-  },
-];
 
 function MyApp() {
-  cosnt [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -31,14 +13,16 @@ function MyApp() {
     });
     setCharacters(updated);
   }
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
 
   return (
     <div className="container">
-      <Table characterData={characters} 
-          removeCharacter={removeOneCharacter} />
-      <Form />
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
     </div>
-  )
+  );
 }
 
 export default MyApp;
