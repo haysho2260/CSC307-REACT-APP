e7497cd888d614197a981cd8cce75e1c7a029f5e
Hayley <haysho2260@gmail.com>
2023-10-04 17:47:19 -0700

    add look up users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,6 +33,11 @@ const users = {
        }
     ]
  }
+ const findUserByName = (name) => { 
+    return users['users_list']
+        .filter( (user) => user['name'] === name); 
+}
+
 
 app.use(express.json());
 
@@ -44,8 +49,17 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
-
-
- app.get('/users', (req, res) => {
-    res.send(users);
+//  app.get('/users', (req, res) => {
+//     res.send(users);
+// });
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
